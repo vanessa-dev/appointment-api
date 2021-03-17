4954143ff0421533a83a10fd46c3a89d9aec86e6
Vanessa Santana <vanessah4money@gmail.com>
2021-03-17 15:07:40 -0300

    Appointment Model

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,13 +1,11 @@
 import {Router} from 'express';
 import {uuid} from 'uuidv4';
 import {startOfHour, parseISO, isEqual} from 'date-fns';
+import Appointment from '../models/Appointment';
+
 
 const appointmentsRouter= Router();
-interface Appointment{
-    id: string;
-    provider: string,
-    date: Date
-}
+
 const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/',(request,response)=>{
@@ -19,11 +17,8 @@ appointmentsRouter.post('/',(request,response)=>{
     if(findAppointmentInSameDate){
         return response.status(400).json({message:"This appointment is already booked!"})
     }
-    const appointment ={
-        id:uuid(),
-        provider,
-        date: parsedDate
-    }
+    
+    const appointment = new Appointment(provider,parsedDate);
     appointments.push(appointment);
     return response.json(appointment);
 });
