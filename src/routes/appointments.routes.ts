d51b7dd25f302b8566010f920efedf4f80c0e664
Vanessa Santana <vanessah4money@gmail.com>
2021-03-17 21:50:10 -0300

    Services e Solid

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,25 +1,23 @@
 import {Router} from 'express';
-import {uuid} from 'uuidv4';
-import {startOfHour, parseISO, isEqual} from 'date-fns';
+import { parseISO} from 'date-fns';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-
+import CreateAppointmentService from '../services/createAppointmentService';
 
 const appointmentsRouter= Router();
 const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/',(request,response)=>{
-    const {provider,date} = request.body;
-     
-    const parsedDate = startOfHour(parseISO(date));
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
-
-    if(findAppointmentInSameDate){
-        return response.status(400).json({message:"This appointment is already booked!"})
+    try{
+        const {provider,date} = request.body;
+        const parsedDate = parseISO(date);
+        const createAppointment = new CreateAppointmentService(appointmentsRepository);
+        const appointment = createAppointment.execute({date: parsedDate,provider})
+        return response.json(appointment);
+    }catch(err){
+        return response.status(400).json({error: err.message});
     }
- 
-    const appointment = appointmentsRepository.create({provider,date:parsedDate});
-    return response.json(appointment);
+   
 });
 
 appointmentsRouter.get('/',(request,response)=>{
