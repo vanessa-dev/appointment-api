4a93db4334b07c350a59b11087966e4c272b3e35
Vanessa Santana <vanessah4money@gmail.com>
2021-03-25 23:24:23 -0300

    feat: create appointment table

---
+++ src/database/migrations/1616724423296-CreateAppointments.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateAppointments1616724423296 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+          new Table({
+            name:'appointments',
+            columns:[
+                {
+                    name:'id',
+                    type:'varchar',
+                    isPrimary:true,
+                    generationStrategy:'uuid',
+                },  
+                {
+                    name:'provider',
+                    type:'varchar',
+                    isNullable:false
+                },
+                {
+                    name:'date',
+                    type:'timestamp with time zone',
+                    isNullable:false
+
+                }
+            ]  
+          })
+      );  
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments');
+    }
+
+}
