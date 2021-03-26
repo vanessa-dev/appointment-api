a378e175dc904d5e8b820f76ada53281dabf3218
Vanessa Santana <vanessah4money@gmail.com>
2021-03-26 13:15:22 -0300

    feat: crypto senha

--- src/services/createUserService.ts
+++ src/services/createUserService.ts
@@ -1,4 +1,5 @@
 import {getRepository} from 'typeorm';
+import {hash} from 'bcryptjs';
 import User from '../models/User';
 interface Request{
     name:string;
@@ -17,11 +18,11 @@ class CreateUserService{
         if(checkUserExists) {
             throw new Error('Email address already used.')
         }
-
+        const hashedPassword = await hash(password,8);
         const user = usersRepository.create({
             name,
             email,
-            password
+            password:hashedPassword
         });
 
         await usersRepository.save(user);
