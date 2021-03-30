18f34088fa52e03adb6ddfa524678a768c8ca6a0
Vanessa Santana <vanessah4money@gmail.com>
2021-03-30 10:09:49 -0300

    feat: show the images

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
+
 import'./database';
 require('dotenv').config()
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.get('/',(request, response)=>{
